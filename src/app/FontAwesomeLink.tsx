0651f4bf07dee095ca53f149b2521febb8f20ba6
ultra1tech <ultratechdevpmnt@gmail.com>
2025-06-21 17:03:26 +0000

    Add src/app/FontAwesomeLink.tsx 1750525405410

---
+++ src/app/FontAwesomeLink.tsx
@@ -0,0 +1,8 @@
+export default function FontAwesomeLink() {
+  return (
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+    />
+  );
+}
